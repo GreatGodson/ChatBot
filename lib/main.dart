676552b054c856f6d25627228acd54eba3c5030f
GreatGodson <greatgodsonokezie@yahoo.com>
2023-02-17 08:41:39 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,19 @@ import 'package:chat_gpt/app/shared/utils/theme/theme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+// mport '../flutter_flow/flutter_flow_animations.dart';
+// import '../flutter_flow/flutter_flow_theme.dart';
+// import '../flutter_flow/flutter_flow_util.dart';
+// import 'package:easy_debounce/easy_debounce.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/scheduler.dart';
+// import 'package:flutter_animate/flutter_animate.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:provider/provider.dart';
+//
+// import 'home_page_model.dart';
+// export 'home_page_model.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EnvSetup.init();
